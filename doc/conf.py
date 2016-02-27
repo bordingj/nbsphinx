95fa74671870c3ef06425e6af626ebca27f85337
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 13:30:34 +0100

    DOC: Switch to agogo theme

--- doc/conf.py
+++ doc/conf.py
@@ -62,6 +62,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'agogo'
 
 # -- Options for LaTeX output ---------------------------------------------
 
